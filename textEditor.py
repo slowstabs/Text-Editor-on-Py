bbf6196091738bebee55e09e48556134e6f05bac
slowstabs <vikhyat97@gmail.com>
2024-12-30 10:24:40 +0530

    Added undo and redo commands onto the menu bar

--- textEditor.py
+++ textEditor.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter.filedialog import *
 from tkinter.messagebox import *
 
+
 filename = None
 
 def newFile():
@@ -17,7 +18,7 @@ def saveFile():
     f.close()
 
 def saveAs():
-    f =  asksaveasfile(mode = "w", defaultextension='.txt')
+    f = asksaveasfile(mode = "w", defaultextension='.txt')
     t = text.get(0.0, END)
     
     try:
@@ -32,13 +33,25 @@ def openFile():
     text.delete(0.0, END)
     text.insert(0.0, t)
     
+def undo():
+    try:
+        text.edit_undo()
+    except TclError:
+        showerror("Nothing to undo!")
+
+def redo():
+    try:
+        text.edit_redo()
+    except TclError:
+        pass
+    
 root = Tk()
 root.title("My text editor")
 
 root.minsize(width=500, height=400)
 root.maxsize(width=700, height=700)
 
-text = Text(root, width=500, height=500)
+text = Text(root, width=500, height=500, undo=True)
 text.pack()
 
 menubar = Menu(root)
@@ -51,6 +64,11 @@ filemenu.add_separator()
 filemenu.add_command(label="Quit", command=root.quit)
 
 menubar.add_cascade(label="File", menu=filemenu)
+
+editmenu = Menu(root)
+editmenu.add_command(label="Undo", command= undo)
+editmenu.add_command(label="Redo", command= redo)
+menubar.add_cascade(label="Edit", menu=editmenu)
 
 root.config(menu=menubar)
 root.mainloop()
